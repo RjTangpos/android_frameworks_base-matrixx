Evolution-X-AOSP/frameworks_base

SystemUI: BatteryMeterView: Adapt to Android 14 QPR1
* mCharging -> isCharging()

packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -343,25 +343,25 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mLandscapeDrawableOrigami.setBatteryLevel(mLevel);
             updatePercentText();
         }
-        if (mCharging != pluggedIn) {
-            mCharging = pluggedIn;
-            mAccessorizedDrawable.setCharging(mCharging);
-            mCircleDrawable.setCharging(mCharging);
-            mFullCircleDrawable.setCharging(mCharging);
-            mRLandscapeDrawable.setCharging(mCharging);
-            mLandscapeDrawable.setCharging(mCharging);
-            mRLandscapeDrawableStyleA.setCharging(mCharging);
-            mLandscapeDrawableStyleA.setCharging(mCharging);
-            mRLandscapeDrawableStyleB.setCharging(mCharging);
-            mLandscapeDrawableStyleB.setCharging(mCharging);
-            mLandscapeDrawableBuddy.setCharging(mCharging);
-            mLandscapeDrawableLine.setCharging(mCharging);
-            mLandscapeDrawableMusku.setCharging(mCharging);
-            mLandscapeDrawablePill.setCharging(mCharging);
-            mLandscapeDrawableSignal.setCharging(mCharging);
-            mLandscapeDrawableiOS15.setCharging(mCharging);
-            mLandscapeDrawableiOS16.setCharging(mCharging);
-            mLandscapeDrawableOrigami.setCharging(mCharging);
+        if (mPluggedIn != pluggedIn) {
+            mPluggedIn = pluggedIn;
+            mAccessorizedDrawable.setCharging(isCharging());
+            mCircleDrawable.setCharging(isCharging());
+            mFullCircleDrawable.setCharging(isCharging());
+            mRLandscapeDrawable.setCharging(isCharging());
+            mLandscapeDrawable.setCharging(isCharging());
+            mRLandscapeDrawableStyleA.setCharging(isCharging());
+            mLandscapeDrawableStyleA.setCharging(isCharging());
+            mRLandscapeDrawableStyleB.setCharging(isCharging());
+            mLandscapeDrawableStyleB.setCharging(isCharging());
+            mLandscapeDrawableBuddy.setCharging(isCharging());
+            mLandscapeDrawableLine.setCharging(isCharging());
+            mLandscapeDrawableMusku.setCharging(isCharging());
+            mLandscapeDrawablePill.setCharging(isCharging());
+            mLandscapeDrawableSignal.setCharging(isCharging());
+            mLandscapeDrawableiOS15.setCharging(isCharging());
+            mLandscapeDrawableiOS16.setCharging(isCharging());
+            mLandscapeDrawableOrigami.setCharging(isCharging());
             updateShowPercent();
             updatePercentText();
         }
@@ -452,7 +452,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         mEstimateText = null;
 
-        if (mBatteryEstimateFetcher != null && mShowPercentMode == MODE_ESTIMATE && !mCharging) {
+        if (mBatteryEstimateFetcher != null && mShowPercentMode == MODE_ESTIMATE && !isCharging()) {
             mBatteryEstimateFetcher.fetchBatteryTimeRemainingEstimate(
                     (String estimate) -> {
                 if (mBatteryPercentView == null) {
@@ -473,7 +473,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
             // to load its emoji colored variant with the uFE0E flag
             String bolt = "\u26A1";
-            CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
+            CharSequence mChargeIndicator = isCharging() && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
                     mBatteryStyle == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
             String percentText = mChargeIndicator + text;
             // Setting text actually triggers a layout pass (because the text view is set to
@@ -522,7 +522,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
     void updateShowPercent() {
         boolean drawPercentInside = mShowBatteryPercent == 1
-                                    && !mCharging && !mBatteryStateUnknown;
+                                    && !isCharging() && !mBatteryStateUnknown;
         boolean showPercent = mShowBatteryPercent >= 2
                                     || mBatteryStyle == BATTERY_STYLE_TEXT
                                     || mShowPercentMode == MODE_ON;
@@ -547,7 +547,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mLandscapeDrawableiOS16.setShowPercent(drawPercentInside);
         mLandscapeDrawableOrigami.setShowPercent(drawPercentInside);
 
-        if (showPercent || (mBatteryPercentCharging && mCharging)
+        if (showPercent || (mBatteryPercentCharging && isCharging())
                 || mShowPercentMode == MODE_ESTIMATE) {
             if (mBatteryPercentView == null) {
                 mBatteryPercentView = loadPercentView();

RjTangpos/android_frameworks_base-matrixx

Signed-off-by: mukesh22584 <mks22584@gmail.com>

packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -346,19 +346,19 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mFullCircleDrawable.setCharging(isCharging());
             mRLandscapeDrawable.setCharging(isCharging());
             mLandscapeDrawable.setCharging(isCharging());
-            mRLandscapeDrawableStyleA.setCharging(mCharging);
-            mLandscapeDrawableStyleA.setCharging(mCharging);
-            mRLandscapeDrawableStyleB.setCharging(mCharging);
-            mLandscapeDrawableStyleB.setCharging(mCharging);
-            mLandscapeDrawableBuddy.setCharging(mCharging);
-            mLandscapeDrawableLine.setCharging(mCharging);
-            mLandscapeDrawableMusku.setCharging(mCharging);
-            mLandscapeDrawablePill.setCharging(mCharging);
-            mLandscapeDrawableSignal.setCharging(mCharging);
-            mLandscapeDrawableiOS15.setCharging(mCharging);
-            mLandscapeDrawableiOS16.setCharging(mCharging);
-            mLandscapeDrawableOrigami.setCharging(mCharging);
-            mLandscapeDrawableMiUIPill.setCharging(mCharging);
+            mRLandscapeDrawableStyleA.setCharging(isCharging());
+            mLandscapeDrawableStyleA.setCharging(isCharging());
+            mRLandscapeDrawableStyleB.setCharging(isCharging());
+            mLandscapeDrawableStyleB.setCharging(isCharging());
+            mLandscapeDrawableBuddy.setCharging(isCharging());
+            mLandscapeDrawableLine.setCharging(isCharging());
+            mLandscapeDrawableMusku.setCharging(isCharging());
+            mLandscapeDrawablePill.setCharging(isCharging());
+            mLandscapeDrawableSignal.setCharging(isCharging());
+            mLandscapeDrawableiOS15.setCharging(isCharging());
+            mLandscapeDrawableiOS16.setCharging(isCharging());
+            mLandscapeDrawableOrigami.setCharging(isCharging());
+            mLandscapeDrawableMiUIPill.setCharging(isCharging());
             updateShowPercent();
             updatePercentText();
         }
@@ -470,7 +470,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         } else {
             // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
             // to load its emoji colored variant with the uFE0E flag
-            String bolt = "\u26A1\uFE0E";
+            String bolt = "\u26A1";
             CharSequence mChargeIndicator = isCharging() && (mBatteryStyle == BATTERY_STYLE_HIDDEN ||
                     mBatteryStyle == BATTERY_STYLE_TEXT) ? (bolt + " ") : "";
             String percentText = mChargeIndicator + text;